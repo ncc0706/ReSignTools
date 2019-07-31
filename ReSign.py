b6c47ed273a0679f064f24470cc406f473cf0a56
niuyuxian <ncc0706@gmail.com>
2019-07-31 10:57:12 +0800

    python3.x

--- ReSign.py
+++ ReSign.py
@@ -9,19 +9,18 @@
 # 3.执行ReSign.py
 # 
 
-#出现问题：xcrun: error: unable to find utility "PackageApplication", not a developer tool or in PATH，参考下面链接修改
+# 出现问题：xcrun: error: unable to find utility "PackageApplication", not a developer tool or in PATH，参考下面链接修改
 # https://www.cnblogs.com/Crazy-ZY/p/7115076.html
 
 import os
-import sys
 import shutil
 import subprocess
-import commands
+import sys
 
 # 证书名称，必填
 certificate = "BINDOW"
-certificateName = "\"iPhone Distribution: "+certificate+"\""
-#注入的库文件
+certificateName = "\"iPhone Distribution: " + certificate + "\""
+# 注入的库文件
 hookLib = "hook.dylib"
 # 测试库文件使用，不用时，请删掉 ********
 # sourcePath="/Users/wangmeili/Library/Developer/Xcode/DerivedData/hook-dgaqzlolyfusmihfqgtseukonjlr/Build/Products/Debug-iphoneos/hook.dylib"
@@ -29,71 +28,77 @@ hookLib = "hook.dylib"
 # open(targetPath, "wb").write(open(sourcePath, "rb").read())
 
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+
 # 执行系统命令
 def execSys(cmd):
-	print cmd
-	(status, output) = commands.getstatusoutput(cmd)
-	print status,output
+    print(cmd)
+    (status, output) = subprocess.getstatusoutput(cmd)
+    print(status, output)
+
 
 # 获取ipa名称
-def getFileName(path,endstr):
-	files = os.listdir(path)
-	for file in files:
-		if file.endswith(endstr):
-			return file
+def getFileName(path, endstr):
+    files = os.listdir(path)
+    for file in files:
+        if file.endswith(endstr):
+            return file
+
 
 # 检查文件是否存在
 def checkFileExits(path):
-	if not os.path.exists(path):
-		print "error:未找到文件夹："+path
-		sys.exit()
+    if not os.path.exists(path):
+        print("error:未找到文件夹：" + path)
+        sys.exit()
+
 
 # 逆向注入代码
 def hook(appName):
-	shutil.copy(hookLib,  "Payload/"+appName+"/"+hookLib)
-	execSys("./yololib Payload/%s/%s %s"%(appName,appName[:-4],hookLib))
-	execSys("codesign -f -s %s %s" % (certificateName,"Payload/"+appName+"/"+hookLib))
+    shutil.copy(hookLib, "Payload/" + appName + "/" + hookLib)
+    execSys("./yololib Payload/%s/%s %s" % (appName, appName[:-4], hookLib))
+    execSys("codesign -f -s %s %s" % (certificateName, "Payload/" + appName + "/" + hookLib))
+
 
 if __name__ == '__main__':
-	# 清理过名的包
-	execSys("rm *-Resigned.ipa")
-	ipaName = getFileName(os.getcwd(),".ipa")
-	if not ipaName:
-		print "error:请拷贝ipa包到此文件夹下！"
-		sys.exit()
-	resignName = ipaName[:-4]+"-Resigned.ipa"
-	# 清理上次遗留的文件
-	if(os.path.exists("Payload")):
-		shutil.rmtree("Payload")
-	if(os.path.exists("__MACOSX")):
-		shutil.rmtree("__MACOSX")
+    # 清理过名的包
+    execSys("rm *-Resigned.ipa")
+    ipaName = getFileName(os.getcwd(), ".ipa")
+    if not ipaName:
+        print("error:请拷贝ipa包到此文件夹下！")
+        sys.exit()
+    resignName = ipaName[:-4] + "-Resigned.ipa"
+    # 清理上次遗留的文件
+    if (os.path.exists("Payload")):
+        shutil.rmtree("Payload")
+    if (os.path.exists("__MACOSX")):
+        shutil.rmtree("__MACOSX")
 
-	# 解压ipa
-	execSys("unzip "+ipaName)
-	# 程序名称app
-	appName = getFileName("Payload",".app")
-	# 删除原签名文件
-	shutil.rmtree("Payload/"+appName+"/_CodeSignature")
-	# 检查是否有证书相关的信息,
-	checkFileExits("Certificates/"+certificate)
-	checkFileExits("Certificates/"+certificate+"/embedded.mobileprovision")
-	checkFileExits("Certificates/"+certificate+"/entitlements.plist")
-	# 注入脚本(如果不需要，可以去掉这一步骤)
-	hook(appName)
-	# 拷贝文件
-	open("Payload/"+appName+"/embedded.mobileprovision", "wb").write(open("Certificates/"+certificate+"/embedded.mobileprovision", "rb").read())
-	# 重新签名
-	print "Resign start:"
-	entitlements = "Certificates/"+certificate+"/entitlements.plist"
-	appPath = "Payload/"+appName
-	execSys("codesign -v -vvvv -f -s "+certificateName+" --entitlements "+entitlements+" "+appPath)
-	print "build resign ipa:"
-	execSys("xcrun -sdk iphoneos PackageApplication -v "+appPath+" -o "+os.path.join(os.getcwd(),resignName))
-	# 清理垃圾文件
-	if(os.path.exists("Payload")):
-		shutil.rmtree("Payload")
-	if(os.path.exists("__MACOSX")):
-		shutil.rmtree("__MACOSX")
+    # 解压ipa
+    execSys("unzip " + ipaName)
+    # 程序名称app
+    appName = getFileName("Payload", ".app")
+    # 删除原签名文件
+    shutil.rmtree("Payload/" + appName + "/_CodeSignature")
+    # 检查是否有证书相关的信息,
+    checkFileExits("Certificates/" + certificate)
+    checkFileExits("Certificates/" + certificate + "/embedded.mobileprovision")
+    checkFileExits("Certificates/" + certificate + "/entitlements.plist")
+    # 注入脚本(如果不需要，可以去掉这一步骤)
+    hook(appName)
+    # 拷贝文件
+    open("Payload/" + appName + "/embedded.mobileprovision", "wb").write(
+        open("Certificates/" + certificate + "/embedded.mobileprovision", "rb").read())
+    # 重新签名
+    print("Resign start:")
+    entitlements = "Certificates/" + certificate + "/entitlements.plist"
+    appPath = "Payload/" + appName
+    execSys("codesign -v -vvvv -f -s " + certificateName + " --entitlements " + entitlements + " " + appPath)
+    print("build resign ipa:")
+    execSys("xcrun -sdk iphoneos PackageApplication -v " + appPath + " -o " + os.path.join(os.getcwd(), resignName))
+    # 清理垃圾文件
+    if (os.path.exists("Payload")):
+        shutil.rmtree("Payload")
+    if (os.path.exists("__MACOSX")):
+        shutil.rmtree("__MACOSX")
 
-	print "Resign finish!"
-
+    print("Resign finish!")
